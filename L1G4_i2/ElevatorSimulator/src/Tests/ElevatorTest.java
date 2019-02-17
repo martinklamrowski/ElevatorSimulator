365a1ee6f04e1a91c452700f4e8cd955204640f3
tashfiqakhand <TashfiqAkhand@cmail.carleton.ca>
2019-02-16 20:17:06 -0500

    almost done tests

--- L1G4_i2/ElevatorSimulator/src/Tests/ElevatorTest.java
+++ L1G4_i2/ElevatorSimulator/src/Tests/ElevatorTest.java
@@ -1,51 +1,61 @@
 /*
  * @author Tashfiq Akhand
+ * Test Case for ElevatorSystem
  */
 
 package Tests;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import ElevatorSystem.*;
 import junit.framework.*;
 
 public class ElevatorTest extends TestCase{
+	//declaring instance variables
 	private Elevator e;
 	private ElevatorDirection direction;
 	private ElevatorStatus status;
 	private ElevatorControl control;
 
+	//Initializing the objects being tested
 	public void setUp() throws Exception {
 		
 		e = new Elevator("4", direction.E_UP);
-		control = new ElevatorControl();
+		control = new ElevatorControl(5000, 3, "4");
 	}
-
+	
+	//Closing the objests being tested
 	public void tearDown() throws Exception {
 		e = null;
 		control = null;
 	}
 
+	//testing all return type methods in Elevator.java and the toInt(String) method in the ElevatorControl.java
 	public void test() {
 		assertEquals("4", e.getCurrentFloor());
 		assertEquals(4, e.getIntFloor());
 		assertEquals(ElevatorDirection.E_UP, e.getDirection());
-		Elevator e2 = new Elevator("4", direction.E_HOLD);
+		Elevator e2 = new Elevator("4", direction.E_HOLD); //creating second elevator object
 		assertEquals(ElevatorStatus.E_IN_USE, e.getStatus());
 		assertEquals(4, control.toInt("4"));
 		
-		String a = "0";
-		byte b[] = a.getBytes();
-		String ans[] = {"0"};
-		assertEquals(ans, control.packetToString(b));
+//		String a = "0";
+//		byte b[] = a.getBytes();
+//		String ans = b.toString();
+//		String[] msg = ans.replaceFirst("\0", "").split("\0");
+//		assertEquals(msg, control.packetToString(b));
 	}
 	
-	//wont work due to different host IP
-//	@Test
-//	public void testcreatePacket() throws UnknownHostException {
-//		DatagramPacket dp = null;
-//		String data = "\0" + "1" + "\0" + "code" + "\0";
-//		dp = new DatagramPacket(data.getBytes(), data.getBytes().length, InetAddress.getLocalHost(), 5);
-//		assertEquals(dp, control.createPacket("1", "code", 5));
-//	}
+	//Testing the created packeted 
+	public void TestcreatePacketData() throws UnknownHostException {
+		
+		String data = "\0" + "1" + "\0" + "3" + "\0"; //packetType = 1
+		byte[] dataB = data.getBytes(); //getting bytes from the data string
+		DatagramPacket packet = new DatagramPacket(data.getBytes(), data.getBytes().length, InetAddress.getLocalHost(), 5000);
+		assertEquals(packet, control.createPacket("1", "3", 5000));
+	}
 	
 	
 
--- L1G4_i2/ElevatorSimulator/src/Tests/FloorTest.java
+++ L1G4_i2/ElevatorSimulator/src/Tests/FloorTest.java
@@ -1,67 +1,69 @@
 /*
  * @author Tashfiq Akhand
+ * Test Case for FloorSystem 
  */
 
 package Tests;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.io.*;
 import FloorSystem.*;
 import FloorSystem.FloorSubsystem.Direction;
 import junit.framework.*;
 
 public class FloorTest extends TestCase{
+	//declaring instance variables
 	private Floor floor;
 	private FloorSubsystem fs;
+	private UserInputHandler uih;
 	
+	//Initializing Objects being tested
 	public void setUp() throws Exception {
-		floor = new Floor(5, 2, true, false);
+		floor = new Floor(2, 5, true, false);
 		fs = new FloorSubsystem();
+		uih = new UserInputHandler(fs);
 	}
 
+	//Closing objects being tested
 	public void tearDown() throws Exception {
 		floor = null;
 		fs = null;
 	}
 
-	public void test() {
+	//testing Floor class
+	public void testFloor() {
 		int floorNo = 5;
 		int numElev = 2;
 		assertEquals(floorNo, floor.getFloorNum());
 		assertEquals(numElev, floor.getElevNum());
-		floor.setUpLampOn();
-		assertTrue(floor.upLamp);
-		floor.setDownLampOn();
-		assertTrue(floor.downLamp);
-		floor.setUpLampOff();
-		assertFalse(floor.upLamp);
-		floor.setDownLampOff();
-		assertFalse(floor.downLamp);
-		
 	}
 	
 	public void TestparseInputFile() throws FileNotFoundException, IOException {
-		fs.parseInputFile("00:00:45:674 1 UP 6");
+		//fs.parseInputFile("00:00:45:674 1 UP 6");
 	}
-	
-	public void TestgetInputData() {
-		Direction d = null;
-		Object[] obj = new Object[7];
-		obj[0] = 00;
-		obj[1] = 00;
-		obj[2] = 45;
-		obj[3] = 674;
-		obj[4] = 1;
-		obj[5] = d.UP;
-		obj[6] = 6;
-		String s = "00:00:45:674 1 UP 6";
-		assertEquals(obj, fs.getInputData(s));
-	}
+//	
+//	//Testing getInputData(String) method in UserInputHandler class
+//	public void TestgetInputData() {
+//		Direction d = null;
+//		Object[] obj = new Object[7]; //initializing array of objects
+//		obj[0] = 00;
+//		obj[1] = 00;
+//		obj[2] = 45;
+//		obj[3] = 674;
+//		obj[4] = 1;
+//		obj[5] = d.UP;
+//		obj[6] = 6;
+//		String s = "00:00:45:674 1 UP 6";
+//		assertEquals(obj, uih.getInputData(s));
+//	}
 	
 	public void TestcreatePacketData() {
 		String ins = "3";
-		String data = "\0" + fs.ACK + "\0" + ins + "\0";
+		String data = "\0" + 1 + "\0" + ins + "\0";
 		byte[] dataB = data.getBytes();
 		assertEquals(dataB, fs.createPacketData(1, "3"));
 	}
@@ -73,5 +75,12 @@ public class FloorTest extends TestCase{
 		assertEquals(ans, fs.readPacketData(b));
 	}
 	
-
+//	public void testReceive() throws SocketException {
+//		String s = "Group4";
+//		byte msg[] = s.getBytes();
+//		DatagramPacket packet = new DatagramPacket(msg, msg.length);
+//		DatagramSocket socket = new DatagramSocket();
+//		assertEquals(packet, fs.receive(socket, msg));
+//	}
+	
 }
